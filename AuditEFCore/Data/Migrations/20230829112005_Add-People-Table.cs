b9a20ad11304177b7d989ed96ded4fee423afbe7
donga-93 <70507672+donga-93@users.noreply.github.com>
2023-08-29 17:32:38 +0400

    first commit

---
+++ AuditEFCore/Data/Migrations/20230829112005_Add-People-Table.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AuditEFCore.Data.Migrations
+{
+    public partial class AddPeopleTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "People",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreatedByUser = table.Column<string>(nullable: true),
+                    CreatedDate = table.Column<DateTimeOffset>(nullable: false),
+                    ModifiedByUser = table.Column<string>(nullable: true),
+                    ModifiedDate = table.Column<DateTimeOffset>(nullable: false),
+                    CustomerName = table.Column<string>(nullable: false),
+                    CommentOne = table.Column<string>(nullable: false),
+                    SendData = table.Column<DateTime>(nullable: false),
+                    CommentTwo = table.Column<string>(nullable: false),
+                    EmployeeName = table.Column<string>(nullable: false),
+                    CommentThree = table.Column<string>(nullable: false),
+                    Category = table.Column<string>(nullable: false),
+                    ContactType = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_People", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "People");
+        }
+    }
+}
---
+++ AuditEFCore/Models/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuditEFCore.Models
+{
+    public class Person: Entity
+    {
+        [Required(ErrorMessage = "შეიყვანე მომხმარებლის სახელი ..")]
+        public string CustomerName { get; set; }
+        [Required(ErrorMessage = "შეიყვანე კომენტარი ..")]
+        public string CommentOne { get; set; }
+        public DateTime SendData { get; set; }
+        [Required(ErrorMessage = "შეიყვანე კომენტარი ..")]
+        public string CommentTwo { get; set; }
+        [Required(ErrorMessage = "შეიყვანე თანამშრომლის სახელი ..")]
+        public string EmployeeName { get; set; }
+        [Required(ErrorMessage = "შეიყვანე კომენტარი ..")]
+        public string CommentThree { get; set; }
+        [Required(ErrorMessage = "შეიყვანე შეტყობინების კატეგორია ..")]
+        public string Category { get; set; }
+        [Required(ErrorMessage = "შეიყვანე საკონტაქტო ინფორმაცია ..")]
+        public string ContactType { get; set; }
+    }
+}
